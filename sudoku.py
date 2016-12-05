14eb0d4eebf76fe3a1bce4a837a02cbb93329cdf
Nisarg <nisarg.ujjainkar@iitgn.ac.in>
2016-12-05 11:19:41 +0530

    branch1

--- sudoku.py
+++ sudoku.py
@@ -13,45 +13,47 @@ def sudoku(name):
     listk=[]
     while i<10:
         listr=[]
-        listbox=[]
+        for k in range(1,10):
+            if k!=j:
+                if a[i][k]!=0:
+                    listr.append(a[i][k])
         listc=[]
         for k in range(1,10):
-            if a[i][k]!=0:
-                listr.append(a[i][k])
-            if a[k][j]!=0:
-                listc.append(a[k][j])
+            if k!=i:
+                if a[k][j]!=0:
+                    listc.append(a[k][j])
         listbox=listb.listb(a,i,j)
         listu=union.union3(listr,listc,listbox)
         listv=complement.complement(listu)
-        if a[i][j]==0 or ([i,j] in listk):
+            
+        if ((a[i][j]==0) or ([i,j] in listk)):
             if [i,j] not in listk:
                 listk.append([i,j])
-            a[i][j]=1
-            while a[i][j]<11:
-                error=errchk.errchk(a[i][j],listv)
-                if error:
-                    a[i][j]+=1
-                    if a[i][j]==10:
-                        a[i][j]=0
-                        j=listk[len(listk)-1][1]
-                        i=listk[len(listk)-1][0]
-                        listk.pop(-1)
-                        a[i][j]+=1
-                        break
-                else:
+            k=a[i][j]+1
+            while(k<10):
+                print k
+                err=errchk.errchk(k,listv)
+                if err:
+                    a[i][j]=k
                     j+=1
                     if j==10:
-                        j=1
                         i+=1
-                    break
-
+                        j=1
+                        break
+                else:
+                    k+=1
+                    if (k==10):
+                        a[i][j]=0
+                        i=listk[-1][0]
+                        j=listk[-1][0]
+                        listk.pop(-1)
+                        break
         else:
             j+=1
             if j==10:
-                j=1
                 i+=1
-            
-
+                j=1
         output.output(a)
+                
             
             
