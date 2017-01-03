3ca94f70599a42d51007edfb3cb5f7fda98d806d
Nisarg <nisarg.ujjainkar@iitgn.ac.in>
2017-01-03 13:05:07 +0530

    a

--- sudoku.py
+++ sudoku.py
@@ -15,39 +15,36 @@ def sudoku(name):
         for j in a[i]:
             if a[i][j]==0:
                 listk.append([i,j])
-    i=listk[0][0]
-    j=listk[0][1]
     u=0
+    print listk[u][0]
     while u<len(listk):
         listr=[]
         listbox=[]
         listc=[]
+        x=listk[u][0]
+        y=listk[u][1]
         for k in range(1,10):
-            if a[i][k]!=0:
-                listr.append(a[i][k])
-            if a[k][j]!=0:
-                listc.append(a[k][j])
-        listbox=listb.listb(a,i,j)
+            if a[x][k]!=0:
+                listr.append(a[x][k])
+            if a[k][y]!=0:
+                listc.append(a[k][y])
+        listbox=listb.listb(a,x,y)
         listu=union.union3(listr,listc,listbox)
         listv=complement.complement(listu)
-        a[i][j]+=1
-        while a[i][j]<=10:
-            error=errchk.errchk(a[i][j],listv)
+        a[x][y]+=1
+        while a[x][y]<=10:
+            error=errchk.errchk(a[x][y],listv)
             if error:
-                a[i][j]+=1
+                a[x][y]+=1
             else:
                 u+=1
-                i=listk[u][0]
-                j=listk[u][1]
+                x=listk[u][0]
+                y=listk[u][1]
                 break
                     
-        if i<10 and j<10:
-            if a[i][j]==11:
-                a[i][j]=0
-                u-=1
-                i=listk[u][0]
-                j=listk[u][1]
-            
+        if a[x][y]==11:
+            a[x][y]=0
+            u-=1
         
             
     output.output(a)
